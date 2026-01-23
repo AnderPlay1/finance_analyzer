34dad21fdf9f6f36afee9caa07b2cc49db48b51f
Daniel <daniel.ast2007@gmail.com>
2026-01-23 15:50:37 +0300

    fixed parser

--- scripts/parser.py
+++ scripts/parser.py
@@ -11,12 +11,17 @@ def parse_cities():
     users = pd.read_csv("../data/users_data.csv", encoding="cp1251", sep=';')
     users = users[users["citizenship_country_nm"] == "РФ"]
     users = users[users["lvn_state_nm"] != "0"]
-    users = users.drop(columns=["gender_cd", "citizenship_country_nm", "first_bank_product_date", "first_session_dttm", "risk_level_cd"])
+    users = users.drop(columns=["Unnamed: 0", "gender_cd", "citizenship_country_nm", "first_bank_product_date", "first_session_dttm", "risk_level_cd"])
+
     with open("../data/all_cities.txt", "r") as file:
         all_cities = file.read().splitlines()
+    
     for index, row in users.iterrows():
         for j in all_cities:
             if j in row["lvn_state_nm"]: users.at[index, "lvn_state_nm"] = j
-    users.rename({"party_rk": "user_id", "monthly_income_amt": "income", "lvn_state_nm": "region"}, axis="columns")
+    
+    users.rename(columns={"party_rk": "user_id", "monthly_income_amt": "income", "lvn_state_nm": "region"}, inplace=True)
+    print(users)
     users.to_sql("users", engine, if_exists="append")
 
+parse_cities()
